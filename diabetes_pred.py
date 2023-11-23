2fc84cd6b0ef67c4d53a18b7d1fac740257ea1e2
Solomon216 <122361545+Solomon216@users.noreply.github.com>
2023-11-23 16:20:34 +0530

    Update diabetes_pred.py

--- diabetes_pred.py
+++ diabetes_pred.py
@@ -1,6 +1,7 @@
+!pip install scikit-learn
+
 import pandas as pd
 import numpy as np
-import scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import streamlit as st
